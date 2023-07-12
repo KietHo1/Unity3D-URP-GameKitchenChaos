d95d804095ac46398fa47636c1dd7220d430a2fe
KietHo1 <kiethostartup1999@gmail.com>
2023-07-12 14:03:26 +0700

    Options, Audio Levels

--- Assets/Scripts/UI/GamePauseUI.cs
+++ Assets/Scripts/UI/GamePauseUI.cs
@@ -8,14 +8,20 @@ public class GamePauseUI : MonoBehaviour
 {
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button mainMenuButton;
+    [SerializeField] private Button optionsButton;
 
     private void Awake()
     {
         resumeButton.onClick.AddListener(() => {
             KitchenGameManager.Instance.TogglePauseGame();
         });
+
         mainMenuButton.onClick.AddListener(() => {
             Loader.Load(Loader.Scene.MainMenuScene);
+        });
+
+        optionsButton.onClick.AddListener(() => {
+            OptionsUI.Instance.Show();
         });
     }
 
