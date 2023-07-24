054ab9cc71495c6fc942a48e6575d76e30f77b35
KietHo1 <kiethostartup1999@gmail.com>
2023-07-24 10:26:14 +0700

    Sync Kitchen Object Parent

--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 using System;
 
 public class ContainerCounter : BaseCounter
@@ -16,8 +17,20 @@ public class ContainerCounter : BaseCounter
             // Player is not carrying something
             KitchenObject.SpawnKitchenObject(kitchenObjectSO, player);
 
-            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+            InteractLogicServerRpc();
         }
         
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void InteractLogicServerRpc()
+    {
+        InteractLogicClientRpc();
+    }
+
+    [ClientRpc]
+    private void InteractLogicClientRpc()
+    {
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
 }
